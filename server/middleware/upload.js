6718119e1d6e52c4e49220ee083255a0350249de
TarasOstasha <tonyjoss1990@gmail.com>
2026-09-07 20:33:48 -0400

    fix image upload directory on Render

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -3,34 +3,32 @@ const path = require('node:path');
 const fs = require('node:fs');
 const multer = require('multer');
 const createHttpError = require('http-errors');
+const { STATIC_PATH } = require('../constants');
 
-
-if (!fs.existsSync(process.env.STATIC_FOLDER)) {
-  fs.mkdirSync(process.env.STATIC_FOLDER, { recursive: true });
-}
+const imagesDir = path.join(STATIC_PATH, 'images');
+fs.mkdirSync(imagesDir, { recursive: true });
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(process.env.STATIC_FOLDER, 'images'));
-    },
-    filename: function (req, file, cb) {
-        const ext = path.extname(file.originalname);
-      cb(null, file.fieldname + '-' + Date.now() + ext);
-    },
-  });
+  destination: function (req, file, cb) {
+    fs.mkdirSync(imagesDir, { recursive: true });
+    cb(null, imagesDir);
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + ext);
+  },
+});
 
-  function fileFilter (req, file, cb) {
-    const MIMETYPE_REG_EXP = /^image\/(gif|png|jpeg|jpg)$/;
-  
-    //   cb(null, MIMETYPE_REG_EXP.test(file.mimetype));
-  
-    if (MIMETYPE_REG_EXP.test(file.mimetype)) {
-      return cb(null, true);
-    }
+function fileFilter(req, file, cb) {
+  const MIMETYPE_REG_EXP = /^image\/(gif|png|jpeg|jpg)$/;
 
-    cb(createHttpError(415, 'Support only jpeg/jpg/png/gif mimetypes'));
+  if (MIMETYPE_REG_EXP.test(file.mimetype)) {
+    return cb(null, true);
   }
 
-  const upload = multer({ storage, fileFilter });
+  cb(createHttpError(415, 'Support only jpeg/jpg/png/gif mimetypes'));
+}
 
-  module.exports.uploadTaskPhoto = upload.any();
+const upload = multer({ storage, fileFilter });
+
+module.exports.uploadTaskPhoto = upload.any();
